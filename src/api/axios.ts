7dc5bfb3e73b98c281a1f7dee8553296998d6298
刘思雯 <siwen.liu@chaitin.com>
2024-06-14 11:24:06 +0800

    feat: axios错误处理;

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { message } from 'antd'
 const instance = axios.create({
   timeout: 1000 * 60 * 5
 })
@@ -14,7 +13,33 @@ instance.interceptors.response.use(
     return response
   },
   (err) => {
-    message.error(err)
+    // 错误处理
+    if (err && err.response) {
+      switch (err.response.status) {
+        case 400:
+          err.message = ''
+          break
+        case 403:
+          err.message = '权限不足: 403'
+          break
+        case 500:
+          err.message = '服务器端出错: 500'
+          break
+        case 502:
+          err.message = '服务器端出错: 502'
+          break
+        case 504:
+          err.message = '服务器端出错: 504'
+          break
+        case 505:
+          err.message = '服务器端出错: 505'
+          break
+        default:
+          err.message = `连接错误 ${err.response.status}`
+      }
+      return err?.response
+    }
+    
     return Promise.reject(err)
   }
 )
